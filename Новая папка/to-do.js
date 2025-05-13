c2abb63489891be6eac68c780455a38896bfc241
alambek <alambek95aydamirov@gmail.com>
2025-05-13 18:20:48 +0300

    tasks

---
+++ Новая папка/to-do.js
@@ -0,0 +1,22 @@
+// let promise = new Promise ((resolve, reject) => {
+//     resolve(data)
+// })
+
+// .then
+
+
+
+// Promise = {
+//     status: '', //pending, fulfilled, rejected
+//     value: 'data'
+// }
+
+// async function print(){
+//     let todos = fetch('data')
+//     let todos1 = await todos.json()
+//     let data = await todos1
+// } (jsinfo.com)
+
+// npm - Node Package Manager (npmjs.com)
+
+
